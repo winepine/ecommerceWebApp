b0e25ae5a3266328bd82e789d87a630269c5a08d
Basit Saeed <69200255+winepine@users.noreply.github.com>
2022-01-02 20:31:12 +0500

    Delete User Addede

--- src/components/AdminComps/DeleteUser.jsx
+++ src/components/AdminComps/DeleteUser.jsx
@@ -1,5 +1,22 @@
+import { useRef, useState } from "react";
 import "../../styles/signin.css";
 const DeleteUser = () => {
+  const [email, setEmail] = useState("");
+  const errorRef = useRef();
+  const deleteCall = async () => {
+    const response = await fetch("/api/delete", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        email: email,
+      }),
+    });
+    const res = await response.json();
+    errorRef.current.innerHTML = JSON.stringify(res.message);
+  };
   return (
     <div className="mainHeader">
       <h1>Remove User</h1>
@@ -7,8 +24,13 @@ const DeleteUser = () => {
         <input
           placeholder="Enter Username"
           className="adminField"
-          type="text"
+          onChange={({ target }) => setEmail(target.value)}
+          value={email}
         />
+        <button onClick={deleteCall} className="adminField">
+          Delete
+        </button>
+        <h2 style={{ color: "rgba(81,110,108)" }} ref={errorRef}></h2>
       </div>
     </div>
   );
